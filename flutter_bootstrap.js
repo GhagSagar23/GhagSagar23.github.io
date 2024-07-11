aa3ecd0259a7af6e38bcf7c237ce3ac3102ceb1a
Sagar Ghag <sagar@meelance.com>
2024-07-11 21:48:55 +0530

    Final Release.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2240627069"
+    serviceWorkerVersion: "2602833430"
   }
 });
